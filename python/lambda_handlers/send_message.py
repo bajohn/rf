fb5b08ed672973f4428ef4280df5344866fe70af
Brendan John <bjohn454@gmail.com>
2020-04-11 21:02:04 -0400

    copy correct code from aws console

--- python/lambda_handlers/send_message.py
+++ python/lambda_handlers/send_message.py
@@ -12,10 +12,14 @@ def handler(event, context):
 
     body_obj = json.loads(event['body'])
     game_id = body_obj['message']['game_id']
-    client = boto3.client('dynamodb')
+    dynamodb = boto3.client('dynamodb')
+    
+    domain = event['requestContext']['domainName']
+    stage = event['requestContext']['stage']
+    endpoint = f'https://{domain}/{stage}'
     gatewayapi = boto3.client("apigatewaymanagementapi", endpoint_url=endpoint)
 
-    get_resp = client.get_item(
+    get_resp = dynamodb.get_item(
         TableName='rf_game',
         Key={
             "game_id": {
@@ -27,12 +31,12 @@ def handler(event, context):
         get_item = get_resp['Item']
         if 'connection_ids' in get_item:
             conn_ids = get_item['connection_ids']['L']
-            dead_conns = []
+            alive_conns = []
             for conn_id_obj in conn_ids:
                 conn_id = conn_id_obj['S']
-                dead_conns.extend(post_to_connection(event, conn_id, '{"msg": "Boom!!"}'))
-            alive_conns = [c for c in conn_ids where c not in dead_conns]
-            put_conn_ids(gatewayapi, game_id, alive_conns)
+                alive_conns.extend(post_to_connection(gatewayapi, conn_id, '{"msg": "Boom!!"}'))
+            # alive_conns = [c for c in conn_ids if c not in dead_conns]
+            put_conn_ids(dynamodb, game_id, alive_conns)
             
         else:
             raise Exception('Missing connection ids?!')
@@ -40,22 +44,21 @@ def handler(event, context):
     return {"statusCode": 200}
 
 
-def post_to_connection(event, connection_id, msg):
-    domain = event['requestContext']['domainName']
-    stage = event['requestContext']['stage']
-    endpoint = f'https://{domain}/{stage}'
-
+def post_to_connection(gatewayapi, connection_id, msg):
 
     try:
         gatewayapi.post_to_connection(ConnectionId=connection_id,
                                          Data=msg.encode('utf-8'))
-        return []
+        return [{"S": connection_id}]
     except:  #GoneException, delete this connection id
-        return [connection_id]
+        logger.log(logging.INFO, f'dead id {connection_id}')
+        return []
 
 
-def put_conn_ids(client, game_id, conn_ids):
-    put_resp = client.put_item(
+
+def put_conn_ids(dynamodb, game_id, conn_ids):
+    logger.log(logging.INFO, conn_ids)
+    put_resp = dynamodb.put_item(
         TableName='rf_game',
         Item={
             "game_id": {
@@ -67,6 +70,7 @@ def put_conn_ids(client, game_id, conn_ids):
             "date": {
                 "S": datetime.now().isoformat()
             }
-
         })
+        
+        
     return put_resp
