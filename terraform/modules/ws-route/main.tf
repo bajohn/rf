1948a2dfc8effa5bed22293caec3bc02ba996b1d
Brendan John <bjohn454@gmail.com>
2020-04-11 21:04:39 -0400

    remove unused tf code

--- terraform/modules/ws-route/main.tf
+++ terraform/modules/ws-route/main.tf
@@ -1,16 +1,16 @@
 # Note- this module is not fully implemented due to 
 # lack of terraform support as of yet.
 
-resource "aws_apigatewayv2_integration" "gw-integration" {
-  api_id           = var.api.id
-  integration_type = "AWS_PROXY"
-
-  connection_type           = "INTERNET"
-  content_handling_strategy = "CONVERT_TO_TEXT"
-  description               = "Lambda example"
-  integration_method        = "POST"
-  integration_uri           = var.lambda.invoke_arn
-  passthrough_behavior      = "WHEN_NO_MATCH"
-}
+#resource "aws_apigatewayv2_integration" "gw-integration" {
+#  api_id           = var.api.id
+#  integration_type = "AWS_PROXY"
+#
+#  connection_type           = "INTERNET"
+#  content_handling_strategy = "CONVERT_TO_TEXT"
+#  description               = "Lambda example"
+#  integration_method        = "POST"
+#  integration_uri           = var.lambda.invoke_arn
+#  passthrough_behavior      = "WHEN_NO_MATCH"
+#}
 
 
